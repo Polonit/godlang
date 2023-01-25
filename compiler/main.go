6cd129620ca82ab04b010db3a12e781e992c063b
Louis-de-Lavenne-de-Choulot <59236850+Polonit@users.noreply.github.com>
2023-01-25 22:42:34 +0100

    sketch of multi type

--- compiler/main.go
+++ compiler/main.go
@@ -8,16 +8,25 @@ package main
 import (
 	"bufio"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
 var (
-	identifiers map[string]string
+	identifiers map[string]variable
+	regexes     map[string]*regexp.Regexp
 )
 
+type variable struct {
+	gtype string
+	value string
+}
+
 func main() {
-	identifiers = make(map[string]string)
+	identifiers = make(map[string]variable)
+	regexes = make(map[string]*regexp.Regexp)
+	regexCompiling()
 	filepath := "src.god"
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -45,33 +54,123 @@ func processLine(line string) {
 
 func evaluateVarAssignment(tokens []string) {
 	// if int str etc...
-	identifiers[tokens[1]] = evaluateExpression(tokens[3:])
+	identifiers[tokens[1]] = variable{gtype: tokens[0], value: evaluateExpression(tokens[3:])}
 }
 
 func evaluateExpression(exp []string) string {
 	if len(exp) == 1 {
 		uni := exp[0]
-		if identifiers[uni] == "" {
+		if identifiers[uni] == (variable{}) {
 			return uni
 		}
-		return identifiers[uni]
+		return identifiers[uni].value
 	}
 	str := ""
-	leftHand := atoi(evaluateExpression([]string{exp[0]}))
-	rightHand := atoi(evaluateExpression(exp[2:]))
-	switch exp[1] {
-	case "+":
-		str += itoa(leftHand + rightHand)
-	case "-":
-		str += itoa(leftHand - rightHand)
-	case "*":
-		str += itoa(leftHand * rightHand)
-	case "/":
-		str += itoa(leftHand / rightHand)
-	case "%":
-		str += itoa(leftHand % rightHand)
+
+	leftHand := evaluateExpression([]string{exp[0]})
+	rightHand := evaluateExpression(exp[2:])
+	// tuple leftHand rightHand
+	tuple := func(leftHand, rightHand string) string {
+		return leftHand + " " + rightHand
 	}
+
+	switch tuple(evaluateType(leftHand), evaluateType(rightHand)) {
+	case "bool bool":
+		evaluateBoolOperator(leftHand, rightHand)
+	case "char char":
+		evaluateStringOperator(leftHand, rightHand)
+	case "int int":
+		evaluateIntOperator(leftHand, rightHand)
+	case "float float":
+		evaluateFloatOperator(leftHand, rightHand)
+	case "string string":
+		evaluateFloatOperator(leftHand, rightHand)
+
+	}
+
+	str += operator(leftHand, rightHand)
+
 	return str
+}
+
+func evaluateBoolOperator(op string) func(string, string) string {
+	switch op {
+	case "&&":
+		return func(leftHand, rightHand string) string { return leftHand && rightHand }
+	case "||":
+		return func(leftHand, rightHand string) string { return leftHand || rightHand }
+	case "==":
+		return func(leftHand, rightHand string) string { return leftHand == rightHand }
+	case "!=":
+		return func(leftHand, rightHand string) string { return leftHand != rightHand }
+	case "^":
+		return func(leftHand, rightHand string) string { return (leftHand && !rightHand) || (!leftHand && rightHand) }
+	}
+	panic("Bool operator evaluation failed")
+}
+
+func evaluateStringOperator(op string) func(string, string) string {
+	switch op {
+	case "+":
+		return func(leftHand, rightHand string) string { return leftHand + rightHand }
+	}
+	panic("String operator evaluation failed")
+}
+
+func evaluateIntOperator(op string) func(int, int) string {
+	switch op {
+	case "+":
+		return func(leftHand, rightHand int) string { return itoa(leftHand + rightHand) }
+	case "-":
+		return func(leftHand, rightHand int) string { return itoa(leftHand - rightHand) }
+	case "*":
+		return func(leftHand, rightHand int) string { return itoa(leftHand * rightHand) }
+	case "/":
+		return func(leftHand, rightHand int) string { return itoa(leftHand / rightHand) }
+	case "%":
+		return func(leftHand, rightHand int) string { return itoa(leftHand % rightHand) }
+	}
+	panic("Int operator evaluation failed")
+}
+
+func evaluateFloatOperator(op string) func(float64, float64) string {
+	switch op {
+	case "+":
+		return func(leftHand, rightHand float64) string { return strconv.FormatFloat(leftHand+rightHand, 'f', 6, 64) }
+	case "-":
+		return func(leftHand, rightHand float64) string { return strconv.FormatFloat(leftHand-rightHand, 'f', 6, 64) }
+	case "*":
+		return func(leftHand, rightHand float64) string { return strconv.FormatFloat(leftHand*rightHand, 'f', 6, 64) }
+	case "/":
+		return func(leftHand, rightHand float64) string { return strconv.FormatFloat(leftHand/rightHand, 'f', 6, 64) }
+	case "%":
+		return func(leftHand, rightHand float64) string {
+			return strconv.FormatFloat(leftHand/100*rightHand, 'f', 6, 64)
+		}
+	}
+	panic("Float operator evaluation failed")
+}
+
+func evaluateType(str string) string {
+	if identifiers[str] == (variable{}) {
+		for key, value := range regexes {
+			if value.MatchString(str) {
+				return key
+			}
+		}
+		panic("No valid type found")
+	}
+	return identifiers[str].gtype
+}
+
+func regexCompiling() {
+	regexes["int"] = regexp.MustCompile("[0-9]+")
+	// char
+	regexes["char"] = regexp.MustCompile("\"[a-zA-Z0-9]\"")
+	regexes["str"] = regexp.MustCompile("\"[a-zA-Z0-9]*\"")
+	regexes["bool"] = regexp.MustCompile("true|false")
+	// float
+	regexes["float"] = regexp.MustCompile("[0-9]+.[0-9]+")
 }
 
 func atoi(str string) int {
